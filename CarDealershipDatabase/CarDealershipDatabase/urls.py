340735a07a5e3b8acb6b70cd16a43236af2c296f
Anthony <noceria198@potsdam.edu>
2017-12-04 17:43:53 -0500

    OMG why

--- CarDealershipDatabase/CarDealershipDatabase/urls.py
+++ CarDealershipDatabase/CarDealershipDatabase/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^home/', include('home.urls')),
+    url(r'^home/',include('home.urls')),
+    url(r'^login/', include('login.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('login.urls')),
+
 ]
+
