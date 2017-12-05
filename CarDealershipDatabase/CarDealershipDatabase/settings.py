35bbc280bab244f51f3481ca51a5e89ded94a843
Anthony <noceria198@potsdam.edu>
2017-12-04 21:40:44 -0500

    WE GOT THE PICTURESSSSSSSSSSSSSSSSSSSSSS

--- CarDealershipDatabase/CarDealershipDatabase/settings.py
+++ CarDealershipDatabase/CarDealershipDatabase/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -123,3 +124,4 @@ USE_TZ = True
 LOGIN_REDIRECT_URL = '/home/'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- CarDealershipDatabase/CarDealershipDatabase/urls.py
+++ CarDealershipDatabase/CarDealershipDatabase/urls.py
@@ -17,6 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', auth_views.login, {'template_name': 'login.html'}, name='login'),
@@ -25,4 +28,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
